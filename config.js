55ca8a8ea62064105e70e791467f237eee7e81e0
Djalma  Bina <djbina2000@yahoo.com.br>
2017-08-26 12:34:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.__config = path.join(__dirname, '/');
 const K_IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  K_SERVER_PATH: process.env.SERVER_PATH || '',
+  K_SERVER_PATH: process.env.SERVER_PATH || 'https://google-map-react-examples.herokuapp.com',
   K_USE_PRERENDER: process.env.USE_PRERENDER === undefined ? K_IS_PRODUCTION : process.env.USE_PRERENDER === 'true',
   K_IS_PRODUCTION: K_IS_PRODUCTION,
   K_DEV_RELEASE: path.join(__dirname, '/build'),
